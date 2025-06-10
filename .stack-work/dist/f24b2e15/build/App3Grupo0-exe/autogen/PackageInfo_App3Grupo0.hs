f10b9aa0f0b9e3a3d24e7c0d4e71e2032e8e8296
RodrigoSanchezB <rodsanchez.bastias@gmail.com>
2025-06-10 13:43:08 -0400

    Configuración base para utilizar Haskell

---
+++ .stack-work/dist/f24b2e15/build/App3Grupo0-exe/autogen/PackageInfo_App3Grupo0.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module PackageInfo_App3Grupo0 (
+    name,
+    version,
+    synopsis,
+    copyright,
+    homepage,
+  ) where
+
+import Data.Version (Version(..))
+import Prelude
+
+name :: String
+name = "App3Grupo0"
+version :: Version
+version = Version [0,1,0,0] []
+
+synopsis :: String
+synopsis = ""
+copyright :: String
+copyright = ""
+homepage :: String
+homepage = "https://github.com/RodrigoSanchezB/App3Grupo0#readme"
